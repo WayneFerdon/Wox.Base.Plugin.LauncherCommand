74dd6b24e6e3bb838ecc980f20d3991e91dabc01
wayneferdon <wayneferdon@hotmail.com>
2022-10-05 18:41:08 +0800

    Optimizations

--- main.py
+++ main.py
@@ -1,67 +1,36 @@
+# ----------------------------------------------------------------
+# Author: wayneferdon wayneferdon@hotmail.com
+# Date: 2022-02-12 06:25:55
+# LastEditors: wayneferdon wayneferdon@hotmail.com
+# LastEditTime: 2022-10-05 18:16:11
+# FilePath: \Wox.Plugin.WoxCommand\main.py
+# ----------------------------------------------------------------
+# Copyright (c) 2022 by Wayne Ferdon Studio. All rights reserved.
+# Licensed to the .NET Foundation under one or more agreements.
+# The .NET Foundation licenses this file to you under the MIT license.
+# See the LICENSE file in the project root for more information.
+# ----------------------------------------------------------------
+
 # -*- coding: utf-8 -*-
-from wox import Wox, WoxAPI
-import re
+from RegexList import *
+from WoxQuery import *
 
-
-class woxConfigs(Wox):
-    @classmethod
+class WoxCommand(WoxQuery):
     def query(cls, queryString):
         IconPath = "./Images/woxIcon.png"
-        exit = {
-            "Title": "Exit",
-            "SubTitle": "退出Wox",
-            "IcoPath": IconPath,
-            "JsonRPCAction": {
-                "method": "Wox.CloseApp",
-                "dontHideAfterAction": False
-            }
-        }
-        restart = {
-            "Title": "Restart Wox",
-            "SubTitle": "重启Wox",
-            "IcoPath": IconPath,
-            "JsonRPCAction": {
-                "method": "Wox.RestarApp",
-                "dontHideAfterAction": False
-            }
-        }
-        settings = {
-            "Title": "Settings",
-            "SubTitle": "设置",
-            "IcoPath": IconPath,
-            "JsonRPCAction": {
-                "method": "Wox.OpenSettingDialog",
-                "dontHideAfterAction": False
-            }
-        }
-        reload = {
-            "Title": "Reload Plugin Data",
-            "SubTitle": "重载插件数据",
-            "IcoPath": IconPath,
-            "JsonRPCAction": {
-                "method": "Wox.ReloadAllPluginData",
-                "dontHideAfterAction": False
-            }
-        }
-        update = {
-            "Title": "Check for Wox Update",
-            "SubTitle": "检查更新",
-            "IcoPath": IconPath,
-            "JsonRPCAction": {
-                "method": "Wox.CheckForNewUpdate",
-                "dontHideAfterAction": False
-            }
-        }
-        result = [exit, restart, settings, reload, update]
-        # pattern = ".*?".join(q)
-        # regex = re.compile(pattern)
-        regex = re.compile(queryString.lower())
-        for item in result:
-            match = regex.search(item["Title"].lower())
-            if not match:
-                result.remove(item)
-        return result
+        exit = WoxResult('Exit', '退出Wox', IconPath, None, "Wox.CloseApp", True).toDict()
+        restart = WoxResult('Restart Exit', '重启Wox', IconPath, None, "Wox.RestarApp", True).toDict()
+        settings = WoxResult('Settings', '设置', IconPath, None, "Wox.OpenSettingDialog", True).toDict()
+        reload = WoxResult('Reload Plugin Data', '重载插件数据', IconPath, None, "Wox.ReloadAllPluginData", True).toDict()
+        update = WoxResult('Check for Wox Update', '检查更新', IconPath, None, "Wox.CheckForNewUpdate", True).toDict()
 
+        resultList = [exit, restart, settings, reload, update]
+        results = list()
+        regex = RegexList(queryString)
+        for result in resultList:
+            if regex.match(result["Title"]):
+                results.append(result)
+        return results
 
 if __name__ == "__main__":
-    woxConfigs()
+    WoxCommand()
